7a6a881a5685f58b48993835b49b6154082f40b9
Vertti Aito <vertti.aito@sartorius.com>
2023-08-14 16:11:16 +0300

    Undo overwritten change

--- android/src/main/java/it/innove/BleManager.java
+++ android/src/main/java/it/innove/BleManager.java
@@ -193,7 +193,15 @@ class BleManager extends ReactContextBaseJavaModule {
             if (getCurrentActivity() == null)
                 callback.invoke("Current activity not available");
             else
-                getCurrentActivity().startActivityForResult(intentEnable, ENABLE_REQUEST);
+                {
+                    try{
+                        getCurrentActivity().startActivityForResult(intentEnable, ENABLE_REQUEST);
+                    }catch(Exception e){
+                        callback.invoke("Current activity not available");
+                    }
+                    
+                }
+                
         } else
             callback.invoke();
     }
